0a6e2712bbfd894659ac30b228f1da8881bcfb0d
Carlos Hugo Escobar Gomez <chescobarg@correo.url.edu.gt>
2024-08-12 09:04:19 -0600

    Agregar archivos de proyecto.

---
+++ Laboratorio 3/Cliente.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Laboratorio_3
+{
+    public class Cliente
+    {
+        public Cliente(string nombre, string correo, string direccion, double descuento)
+        {
+            Nombre = nombre;
+            Correo = correo;
+            Direccion = direccion;
+            Descuento = descuento;
+        }
+
+        public string Nombre { get; set; }
+        public string Correo { get; set; }
+        public string Direccion { get; set; }
+        public double Descuento {  get; set; }
+
+
+        public virtual void MostrarInformacion()
+        {
+            Console.WriteLine("--------CLIENTE--------");
+            Console.WriteLine("\nNombre: " + Nombre);
+            Console.WriteLine("\nCorreo: " + Correo);
+            Console.WriteLine("\nDireccion: " + Correo);
+            if (Descuento > 0) Console.WriteLine($"\nDescuento: {Descuento*100}%");
+        }
+        public virtual double AplicarDescuento(double precio)
+        {
+            if (Descuento == 0)
+            {
+                return precio;
+            }
+            Console.WriteLine("TU membresia tiene un descuento!!");
+            Console.WriteLine("\nPrecio Anterior: Q." + precio);
+            precio = (precio) - (precio * Descuento);
+            Console.WriteLine("Nuevo Precio: Q." + precio);
+            Console.ReadLine();
+            return precio;
+        }
+    }
+}
---
+++ Laboratorio 3/ClienteRegular.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_3
+{
+    public class ClienteRegular:Cliente
+    {
+        public ClienteRegular(string nombre, string correo, string direccion, double descuento) : base(nombre, correo, direccion, descuento) { }
+
+    }
+}
